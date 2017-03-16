88dcf41c5c56eb24d7ab3aa6098faf8853763f29
Dani Adkins <danirenaeadkins12@gmail.com>
2017-03-16 18:02:55 -0500

    completed API for state data - may be minor adjustments

--- capstone_api/admin.py
+++ capstone_api/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from capstone_api import StateData
+from capstone_api.models import StateData
 
 admin.site.register(StateData)
--- capstone_project/urls.py
+++ capstone_project/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from bangazon_api import views
-# from rest_framework import
+from capstone_api import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'state_data', views.StateDataViewSet)
 
 urlpatterns = [
+    url(r'^', include(router.urls)),
     url(r'^admin/', admin.site.urls),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
